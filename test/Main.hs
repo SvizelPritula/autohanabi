b9da3a05cedff6d743d27712b6753f9c0fa20621
Benjamin Swart <Benjaminswart@email.cz>
2025-09-13 12:24:30 +0200

    Add tests for Vec module

--- test/Main.hs
+++ test/Main.hs
@@ -3,6 +3,7 @@ module Main where
 import Control.Exception (evaluate)
 import Test.Hspec (SpecWith, anyException, describe, hspec, it, shouldBe, shouldSatisfy, shouldThrow)
 import Utils (enumerate, indexOf, infinity, removeNth)
+import Vec (Vec (Index, allSame, change, fromIndex, set, toList, vmapWithKey, (!)))
 
 testUtils :: SpecWith ()
 testUtils = describe "Utils" $ do
@@ -19,18 +20,50 @@ testUtils = describe "Utils" $ do
       indexOf (>= 'x') "abcd" `shouldBe` Nothing
 
   describe "removeNth" $ do
-    it "can remove the first element" $ do
-      removeNth 0 "abc" `shouldBe` ('a', "bc")
-    it "can remove an element from the middle" $ do
+    it "can remove an element" $ do
+      removeNth 0 "abcde" `shouldBe` ('a', "bcde")
       removeNth 2 "abcde" `shouldBe` ('c', "abde")
-    it "can remove the last element" $ do
-      removeNth 2 "abc" `shouldBe` ('c', "ab")
+      removeNth 4 "abcde" `shouldBe` ('e', "abcd")
     it "throws an exception when index is out of bounds" $ do
       evaluate (removeNth 3 "abc") `shouldThrow` anyException
+      evaluate (removeNth 4 "abc") `shouldThrow` anyException
 
   describe "infinity" $ do
     it "is very large" $ do
       infinity `shouldSatisfy` (>= 1e300)
+
+data TestEnum = A | B | C deriving (Show, Eq, Ord, Enum, Bounded)
+
+data TestVec a = TestVec a a a deriving (Show, Eq)
+
+instance Vec TestVec where
+  type Index TestVec = TestEnum
+  (!) (TestVec a _ _) A = a
+  (!) (TestVec _ b _) B = b
+  (!) (TestVec _ _ c) C = c
+  fromIndex f = TestVec (f A) (f B) (f C)
+
+testVec :: SpecWith ()
+testVec = describe "Vec" $ do
+  describe "allSame" $ do
+    it "gives all elements same value" $ do
+      allSame "hello" `shouldBe` TestVec "hello" "hello" "hello"
+
+  describe "change" $ do
+    it "changes correct element" $ do
+      change B ('x' :) (TestVec "a" "b" "c") `shouldBe` TestVec "a" "xb" "c"
+
+  describe "set" $ do
+    it "sets correct element" $ do
+      set B "x" (TestVec "a" "b" "c") `shouldBe` TestVec "a" "s" "c"
+
+  describe "vmapWithKey" $ do
+    it "maps everything correctly" $ do
+      vmapWithKey (,) (TestVec 'a' 'b' 'c') `shouldBe` TestVec (A, 'a') (B, 'b') (C, 'c')
+
+  describe "toList" $ do
+    it "returns all elements" $ do
+      toList (TestVec 'a' 'b' 'c') `shouldBe` "abc"
 
 main :: IO ()
 main = hspec $ do
